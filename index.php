cbb6657fed265273e0863ce6668916923301a4f6
Robert <rdtheriault@yahoo.com>
2019-09-23 15:38:53 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        
+        <link href="../hsd.css" rel="stylesheet" type="text/css" />
+       
+        
+    </head>
+    <body>
+        
+      <div style="text-align:center;border-color:white;border:2px solid;padding:10px">
+        <p style="font-size:25px">What do you want to do?</p>
+        <a href="login.php">Login</a><br><br>
+        <a href="http://www.hermiston.k12.or.us">Homepage</a><br><br>
+        <a href="scripts/LogOut.php">Logout</a><br><br>
+      </div>
+        
+       
+
+    </body>
+</html>
